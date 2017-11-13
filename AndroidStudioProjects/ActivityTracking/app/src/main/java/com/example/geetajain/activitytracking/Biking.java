272f29a6bfde4e06e68b2451b093c0a1d10a0fec
CST2335 <jain0022@algonquinlive.com>
2017-11-13 18:43:09 -0500

    commit

--- AndroidStudioProjects/ActivityTracking/app/src/main/java/com/example/geetajain/activitytracking/Biking.java
+++ AndroidStudioProjects/ActivityTracking/app/src/main/java/com/example/geetajain/activitytracking/Biking.java
@@ -3,7 +3,7 @@ package com.example.geetajain.activitytracking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Biking extends AppCompatActivity {
+public class 333333333Biking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
